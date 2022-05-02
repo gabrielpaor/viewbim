1c17e075af9f4ce1c560fa759139030f7aff86c9
gabrielpaor <gabrielpaor07@gmail.com>
2022-05-03 01:01:03 +0800

    web template

--- viewbim/settings.py
+++ viewbim/settings.py
@@ -125,6 +125,9 @@ STATIC_URL = '/static/'
 STATICFILE_DIRS = [
     os.path.join(BASE_DIR, 'static')
 ]
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
